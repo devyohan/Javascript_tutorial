558a47869c332b4253c90fed8fc35317a5f6969a
devyohan <ticonweb@gmail.com>
2018-08-13 16:00:59 +0900

    First helloworld code

---
+++ typescript/helloworld/with_webstorm/hello.ts
@@ -0,0 +1,13 @@
+"use strict";
+class Greeting {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+    sayHello() {
+        return "Hello " + this.greeting;
+    }
+}
+
+let tmp = new Greeting("World!!");
+console.log(tmp.sayHello());
